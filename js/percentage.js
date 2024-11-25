79fdce2d48ff68aa4d60c4269fd660ef8a582220
Sandeep Kumar <135053219+sandeepwebsite@users.noreply.github.com>
2024-11-25 23:28:49 +0530

    Add files via upload

---
+++ js/percentage.js
@@ -0,0 +1,457 @@
+const questions = [
+    {
+        question: "50%",
+        optionA: "1/3",
+        optionB: "1/5",
+        optionC: "3/5",
+        optionD: "1/2",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "33.33%",
+        optionA: "1/6",
+        optionB: "1/3",
+        optionC: "1/11",
+        optionD: "1/9",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "25%",
+        optionA: "1/6",
+        optionB: "1/5",
+        optionC: "1/10",
+        optionD: "1/4",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "16.66%",
+        optionA: "1/16",
+        optionB: "1/8",
+        optionC: "1/6",
+        optionD: "1/7",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "14.28%",
+        optionA: "1/14",
+        optionB: "1/9",
+        optionC: "1/12",
+        optionD: "1/7",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "12.50%",
+        optionA: "1/8",
+        optionB: "1/12",
+        optionC: "1/6",
+        optionD: "1/4",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "11.11%",
+        optionA: "1/11",
+        optionB: "1/3",
+        optionC: "1/9",
+        optionD: "1/6",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "10%",
+        optionA: "1/10",
+        optionB: "1/5",
+        optionC: "1/2",
+        optionD: "1/15",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "9.09%",
+        optionA: "1/9",
+        optionB: "1/3",
+        optionC: "1/6",
+        optionD: "1/11",
+        correctOption: "optionD"
+    },
+
+    {
+        question: `9(1/11)%`,
+        optionA: "1/9",
+        optionB: "1/3",
+        optionC: "1/6",
+        optionD: "1/11",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "8.33%",
+        optionA: "1/8",
+        optionB: "1/3",
+        optionC: "1/12",
+        optionD: "1/6",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "7.69%",
+        optionA: "1/13",
+        optionB: "1/7",
+        optionC: "1/6",
+        optionD: "1/9",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "6.66%",
+        optionA: "1/13",
+        optionB: "1/16",
+        optionC: "1/15",
+        optionD: "1/14",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "6.25%",
+        optionA: "1/6",
+        optionB: "1/14",
+        optionC: "1/25",
+        optionD: "1/16",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "66.66%",
+        optionA: "2/3",
+        optionB: "3/2",
+        optionC: "2/5",
+        optionD: "4/7",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "75%",
+        optionA: "2/3",
+        optionB: "4/7",
+        optionC: "3/4",
+        optionD: "5/4",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "37.5%",
+        optionA: "3/8",
+        optionB: "1/8",
+        optionC: "7/8",
+        optionD: "5/8",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "62.5%",
+        optionA: "3/8",
+        optionB: "1/8",
+        optionC: "5/8",
+        optionD: "7/8",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "14(2/7)%",
+        optionA: "1/14",
+        optionB: "1/8",
+        optionC: "3/7",
+        optionD: "1/7",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "31.25%",
+        optionA: "5/16",
+        optionB: "3/16",
+        optionC: "1/16",
+        optionD: "7/16",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "28.56%",
+        optionA: "1/7",
+        optionB: "3/7",
+        optionC: "4/7",
+        optionD: "2/7",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "41.65%",
+        optionA: "1/12",
+        optionB: "5/12",
+        optionC: "7/12",
+        optionD: "1/41",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "5.55%",
+        optionA: "1/55",
+        optionB: "1/18",
+        optionC: "5/18",
+        optionD: "5/9",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "83.33%",
+        optionA: "3/8",
+        optionB: "3/5",
+        optionC: "5/6",
+        optionD: "5/8",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "8(1/3)%",
+        optionA: "1/12",
+        optionB: "5/12",
+        optionC: "5/6",
+        optionD: "7/12",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "7(9/13)%",
+        optionA: "1/7",
+        optionB: "1/13",
+        optionC: "1/9",
+        optionD: "3/13",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "6(2/3)%",
+        optionA: "1/7",
+        optionB: "1/15",
+        optionC: "1/6",
+        optionD: "3/7",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "9(1/11)%",
+        optionA: "1/9",
+        optionB: "1/13",
+        optionC: "1/11",
+        optionD: "3/11",
+        correctOption: "optionC"
+    },
+    {
+        question: "7(1/7)%",
+        optionA: "1/7",
+        optionB: "1/13",
+        optionC: "1/9",
+        optionD: "1/14",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "6(1/4)%",
+        optionA: "1/16",
+        optionB: "1/13",
+        optionC: "1/9",
+        optionD: "1/14",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "16(2/3)%",
+        optionA: "1/16",
+        optionB: "1/6",
+        optionC: "1/9",
+        optionD: "1/7",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "12(1/2)%",
+        optionA: "1/11",
+        optionB: "1/7",
+        optionC: "1/9",
+        optionD: "1/8",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "8(1/3)%",
+        optionA: "1/12",
+        optionB: "5/12",
+        optionC: "5/6",
+        optionD: "7/12",
+        correctOption: "optionA"
+    }
+
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0
+
+// function for displaying next question in the array to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+   
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
---
+++ js/subtraction_3x3.js
@@ -0,0 +1,58 @@
+// Create a Math question
+// Math question will have a random generated
+// Question Type Multiplicatin question  with random number range 1-10
+// Answer will be the product of the random number range and the random number range
+// User will have to answer question
+// On submit  answer answer will be compared with random generated answer
+// If answer will be correct than score will be incremented
+// If answer will be wrong than score will be decremented
+
+const questionEl = document.getElementById("question");
+const questionFormEl = document.getElementById("questionForm");
+const scoreEl = document.getElementById("score");
+let storedAnswer;
+let score = 0;
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateQuestion = () => {
+  const randomNumber1 = randomNumber(101, 999);
+  const randomNumber2 = randomNumber(100, 1000);
+  // const randomNumber3 = randomNumber(1, 3);
+  const question = `${randomNumber1} - ${randomNumber2} = ?`;
+
+  const answer = randomNumber1 - randomNumber2;
+  return { question, answer };
+};
+
+const showQuestion = () => {
+  const { question, answer } = generateQuestion();
+  questionEl.innerText = question;
+  storedAnswer = answer;
+};
+showQuestion();
+const checkAnswer = (event) => {
+  event.preventDefault();
+  const formData = new FormData(questionFormEl);
+
+  const userAnswer = +formData.get("answer");
+  if (userAnswer === storedAnswer) {
+    // score += 1;
+    document.body.classList.remove('red-wrong');
+      event.target.reset();
+      showQuestion();
+      data++;
+  }
+   else {
+    // score -= 1;
+    // data = data - 1;
+    document.body.classList.add('red-wrong')
+    document.getElementById('answerInput').value = '';
+    document.getElementById('answerInput').placeholder = 'Answer';
+  }
+
+
+  scoreEl.innerText = score;
+  console.log("answer", userAnswer);
+};
